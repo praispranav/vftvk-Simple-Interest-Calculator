8f84db834c514f5c27ea85a8a1aa1f3ed97a8ce3
praispranav <praispranav@gmail.com>
2021-03-30 13:21:08 +0530

    assignment1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
+
+
+function handleSubmit(event)
 {
-    p = document.getElementById("principal").value;
+    event.preventDefault();
+
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("time").value;
+    var r1 = r/100;
+    var t1 = t/12;
+    var si = p*r1*t1;
+    document.getElementById("simpleintrest").innerHTML = ": " + " :  " + si;
     
 }
         
