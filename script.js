e1384c6975c8fd1aedeb0e6ca7d25ddc6959d174
praispranav <praispranav@gmail.com>
2021-03-30 14:11:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,24 @@
 function handleSubmit(event)
 {
     event.preventDefault();
-
+    
     var p = document.getElementById("principal").value;
-    var r = document.getElementById("rate").value;
+    
+    if(p<=0) {
+        
+        document.getElementById("simpleintrest").innerHTML = ": " + " :  " + "Value of Principal Cannot be or zero negative";
+        alert("Value cannot be negative or zero")
+    }
+    else if (p>=0){
+
+    
+        var r = document.getElementById("rate").value;
     var t = document.getElementById("time").value;
     var r1 = r/100;
     var t1 = t/12;
     var si = p*r1*t1;
     document.getElementById("simpleintrest").innerHTML = ": " + " :  " + si;
+    }
     
 }
 
